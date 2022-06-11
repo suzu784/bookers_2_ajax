38ca3dbecd367e4183b63e7024c183fa4d07481a
suzu784 <s.suzuki784@hotmail.com>
2022-06-11 08:01:47 +0000

    コメント機能作成#1

---
+++ db/migrate/20220611055226_add_column_book_comments.rb
@@ -0,0 +1,5 @@
+class AddColumnBookComments < ActiveRecord::Migration[6.1]
+  def change
+    add_column :book_comments, :comment, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_11_031138) do
+ActiveRecord::Schema.define(version: 2022_06_11_055226) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2022_06_11_031138) do
     t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "comment"
   end
 
   create_table "books", force: :cascade do |t|
